4aaee1c0f844af6b73bdb503d73b71c78db6f2cc
vfursov86 <vfursov86@gmail.com>
2025-05-11 21:56:59 +0300

    Improvements

--- Program.cs
+++ Program.cs
@@ -15,15 +15,15 @@ builder.Services.AddScoped<AlbumService>();
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //app.UseHsts();
+    app.UseHsts();
 }
-
-//app.UseHttpsRedirection();
 
 app.UseRouting();
 
