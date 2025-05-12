e531ddb3dc3abad93eb38037a5e44f04e89ff136
vfursov86 <vfursov86@gmail.com>
2025-05-12 20:21:18 +0300

    Kesterl HTTPS settings was added

--- obj/Debug/net8.0/Doomlist.AssemblyInfo.cs
+++ obj/Debug/net8.0/Doomlist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Doomlist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d616ae4530176b19e0852954120093cb4587ceed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+399a3daffbec9559512c74734e04f584973d0838")]
 [assembly: System.Reflection.AssemblyProductAttribute("Doomlist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Doomlist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
